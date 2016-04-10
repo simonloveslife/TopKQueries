a9e2abc4f764ac742503a3f4c14e444ea7c4c64d
gmx9308 <gmx9308@gmail.com>
2016-04-10 12:11:49 -0400

    Finish index function special for root

--- src/cse562/buffalo/edu/Btree.java
+++ src/cse562/buffalo/edu/Btree.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Btree {
 	int order;
 	Node root;
+	int[] range = new int[2];
 	
 	public Btree(int order){
-		this.order = order;
+		this.order = order;// M 
 		this.root.setfather(null);
 		this.root.setkeys(new ArrayList<Integer>());
 		this.root.setsons(new ArrayList<Node>());
@@ -15,8 +16,68 @@ public class Btree {
 	
 	
 	public void index(int n){
+		// index's number less than M, the root is viewed like a leaf
+		if(n<=order){
+			for(int i=0; i<n; i++){
+				ArrayList<Integer> tempkey = new ArrayList<Integer>();
+				tempkey.add(i);
+				root.setkeys(tempkey);
+			}
+			
+		}
+		
+		else{
+			int keyup = n/order;
+			int tkey = 0;
+			while(tkey+keyup<n){
+				root.getkeys().add(keyup);
+				
+				range[0] = tkey;
+				range[1] = tkey+keyup-1;
+				if(keyup<=order){
+					Node newnode = new Node(true, false);
+					newnode.setfather(root);
+					root.getsons().add(newnode);
+					root.getkeys().add(tkey);
+					index(range, newnode);
+				}
+				else{
+					Node newnode = new Node(false, false);
+					newnode.setfather(root);
+					root.getsons().add(newnode);
+					root.getkeys().add(tkey);
+					index(range, newnode);
+				}
+				tkey+=keyup;
+				
+			}
+			root.getkeys().add(keyup);
+			int[] range = new int[2];
+			range[0] = tkey;
+			range[1] = n;
+			if(keyup<=order){
+				Node newnode = new Node(true, false);
+				newnode.setfather(root);
+				root.getsons().add(newnode);
+				root.getkeys().add(tkey);
+				index(range, newnode);
+			}
+			else{
+				Node newnode = new Node(false, false);
+				newnode.setfather(root);
+				root.getsons().add(newnode);
+				root.getkeys().add(tkey);
+				index(range, newnode);
+			}
+			
+			
+		}
+			
 		
 		
+	}
+	
+	public void index(int[] range, Node node){
 		
 	}
 	
