1e8a8c03f2c69f6d74c97b9266759eef0c3a3ab1
gmx9308 <gmx9308@gmail.com>
2016-04-10 11:27:27 -0400

    Add basic function components

--- src/cse562/buffalo/edu/Btree.java
+++ src/cse562/buffalo/edu/Btree.java
@@ -1,5 +1,28 @@
 package cse562.buffalo.edu;
 
-public class Btree {
+import java.util.ArrayList;
 
+public class Btree {
+	int order;
+	Node root;
+	
+	public Btree(int order){
+		this.order = order;
+		this.root.setfather(null);
+		this.root.setkeys(new ArrayList<Integer>());
+		this.root.setsons(new ArrayList<Node>());
+	}
+	
+	
+	public void index(int n){
+		
+		
+		
+	}
+	
+	public Node lookup(int key){
+		
+		
+		return null;
+	}
 }
